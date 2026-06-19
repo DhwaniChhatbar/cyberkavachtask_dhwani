595dcb0ce06bfa7ff625ac0ba2280f46ea169f7d
DhwaniChhatbar <25cs004@charusat.edu.in>
2026-06-19 21:45:28 +0530

    register isue fix

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,56 +1,63 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-  {
-    name: String,
+{
+name: {
+type: String,
+required: true,
+},
 
-    email: {
-      type: String,
-      unique: true,
-    },
+email: {
+  type: String,
+  required: true,
+  unique: true,
+},
 
-    password: String,
+password: {
+  type: String,
+  required: true,
+},
 
-    role: {
-      type: String,
-      enum: [
-        "Admin",
-        "FacultyCoordinator",
-        "StudentCoordinator",
-        "TechCoordinator",
-        "ContentCoordinator",
-        "SocialMediaCoordinator",
-        "Member",
-        "Guest",
-      ],
-      default: "Member",
-    },
+role: {
+  type: String,
+  enum: [
+    "Admin",
+    "Faculty Coordinator",
+    "Student Coordinator",
+    "Tech Coordinator",
+    "Content Coordinator",
+    "Social Media Coordinator",
+    "Member",
+    "Guest",
+  ],
+  default: "Member",
+},
 
-    isApproved: {
-      type: Boolean,
-      default: false,
-    },
+isApproved: {
+  type: Boolean,
+  default: false,
+},
 
-    // OTP SYSTEM
-    otp: {
-      type: String,
-      default: null,
-    },
+// OTP SYSTEM
+otp: {
+  type: String,
+  default: null,
+},
 
-    otpExpiry: {
-      type: Date,
-      default: null,
-    },
+otpExpiry: {
+  type: Date,
+  default: null,
+},
 
-    // Optional reset token
-    resetPasswordToken: {
-      type: String,
-      default: null,
-    },
-  },
-  {
-    timestamps: true,
-  }
+// RESET PASSWORD TOKEN
+resetPasswordToken: {
+  type: String,
+  default: null,
+},
+},
+{
+timestamps: true,
+}
 );
 
 export default mongoose.model("User", userSchema);
